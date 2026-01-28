a5e85b4508111fc4fbf91c3f80453b6a6fa55238
Guido Mantilla Tarazona <raven@macbook-pro-m1-max.local>
2026-01-28 14:42:20 -0500

    updates

---
+++ docker-show.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo \
+&& docker image ls --format  "table {{.Repository}} \t {{.Tag}} \t {{.Size}} \t {{.CreatedSince}}" \
+&& echo \
+&& docker container ls -a --format  "table {{.Image}} \t {{.RunningFor}} \t {{.Names}} \t {{.Status}} \t {{.Networks}} \t {{.Ports}}" \
+&& echo && docker network ls && echo && docker volume ls && echo
